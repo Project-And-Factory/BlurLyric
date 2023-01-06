cbf9f4c8ea10d90028be07b9275f8563fa5cda07
Doge <ss15278205751@foxmail.com>
2023-01-06 10:21:29 +0800

    Update playingList.vue

--- src/views/playingList.vue
+++ src/views/playingList.vue
@@ -58,21 +58,21 @@
                 </div>
                     </div>
                     <div class="linkbox bigger">
-                        <a @click="clean(i)">
+                        <a @click="clean(app.data.player.trackNum + 1+i)">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                 class="bi bi-x" viewBox="0 0 16 16">
                                 <path
                                     d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
                             </svg>
                         </a>
-                        <a @click="upGo(app.data.player.tracks,i)">
+                        <a @click="upGo(app.data.player.tracks,app.data.player.trackNum + 1+i)">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                 class="bi bi-arrow-bar-up" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd"
                                     d="M8 10a.5.5 0 0 0 .5-.5V3.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 3.707V9.5a.5.5 0 0 0 .5.5zm-7 2.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5z" />
                             </svg>
                         </a>
-                        <a @click="downGo(app.data.player.tracks,i)">
+                        <a @click="downGo(app.data.player.tracks,app.data.player.trackNum + 1+i)">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                 class="bi bi-arrow-bar-down" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd"
