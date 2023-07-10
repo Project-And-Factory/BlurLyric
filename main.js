c31d0dd6cb7fefb94fc0bc7895a95beaa9e4224d
Doge <ss15278205751@foxmail.com>
2023-07-10 17:33:23 +0800

    更新了netEaseCloudMusicApi，修复了一些包的问题

--- main.js
+++ main.js
@@ -84,7 +84,7 @@ const createWindow = async () => {
     app.on('activate', () => {
       // On macOS it's common to re-create a window in the app when the
       // dock icon is clicked and there are no other windows open.
-      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+      if (BrowserWindow.getAllWindows().length === 0) {createWindow()}
     })
   })
 
